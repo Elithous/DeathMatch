490b6d971a595964c0e4151e1aea0ad02210085a
yart123 <iaroslav.titov@gmail.com>
2018-02-26 14:23:25 -0700

    merge

---
+++ src/loot/models/Armor.java
@@ -0,0 +1,14 @@
+package loot.models;
+
+import loot.enums.ArmorType;
+
+public class Armor extends Equipment{
+
+	public final ArmorType armorType;
+
+	public Armor(ArmorType armorType) {
+		this.armorType = armorType;
+	}
+	
+	
+}
---
+++ src/loot/models/RevivePotion.java
@@ -0,0 +1,13 @@
+package loot.models;
+
+public class RevivePotion extends Consumable{
+
+	public RevivePotion() {
+		super(); //Needs a health value for parent
+	}
+
+	@Override
+	public void use(Character target) {
+		
+	}
+}
