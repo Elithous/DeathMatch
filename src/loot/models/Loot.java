d06406a608c3e2ce697995bbf95b8227d406defd
Guppy636 <guppy.bacon@yahoo.com>
2018-02-26 13:35:09 -0700

    Added Loot.java and packages
    Added loot.models package, and began stubbing out Loot.java.

---
+++ src/loot/models/Loot.java
@@ -0,0 +1,37 @@
+package loot.models;
+
+import javafx.scene.image.Image;
+
+public class Loot {
+
+	protected String name;
+	protected String description;
+	protected int value;
+	protected Image image;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public int getValue() {
+		return value;
+	}
+	public void setValue(int value) {
+		this.value = value;
+	}
+	public Image getImage() {
+		return image;
+	}
+	public void setImage(Image image) {
+		this.image = image;
+	}
+	
+	
+}
