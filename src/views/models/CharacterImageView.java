4ac07561c4afd27902efd0c7927e668f4ea8b684
josh9harr <josh9harr@gmail.com>
2018-03-05 12:05:52 -0700

    add

--- src/views/models/CharacterImageView.java
+++ src/views/models/CharacterImageView.java
@@ -1,24 +1,31 @@
 package views.models;
 
 import characters.models.Character;
+import javafx.geometry.NodeOrientation;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 
-public class CharacterImageView extends VBox{
+public class CharacterImageView extends VBox {
 
 	private Character character;
-	private ImageView characterImage = new ImageView();
+	private ImageView characterImage;
 	private ProgressBar health = new ProgressBar();
-	
+
 	public CharacterImageView(Character character) {
 		this.setCharacter(character);
-		setCharacterImage(new ImageView(character.getImage()));
-		this.getChildren().addAll(health, characterImage);
-		this.characterImage.fitHeightProperty().bind(this.heightProperty().multiply(.8));
-		this.characterImage.fitWidthProperty().bind(this.widthProperty());
-		this.health.prefHeightProperty().bind(this.heightProperty().multiply(.2));
-		this.health.prefWidthProperty().bind(characterImage.fitWidthProperty());
+		if (character != null) {
+			setCharacterImage(new ImageView(character.getImage()));
+			this.getChildren().addAll(health, characterImage);
+			this.characterImage.setPreserveRatio(true);
+			this.characterImage.fitHeightProperty().bind(this.heightProperty().multiply(.8));
+			this.characterImage.fitWidthProperty().bind(this.widthProperty());
+			this.health.prefHeightProperty().bind(this.heightProperty().multiply(.2));
+			this.health.prefWidthProperty().bind(this.widthProperty().multiply(.35));
+			this.health.setNodeOrientation(NodeOrientation.LEFT_TO_RIGHT);
+			this.health.setStyle("-fx-accent: green;");
+			health.setProgress(.5);
+		}
 	}
 
 	public Character getCharacter() {
@@ -43,5 +50,9 @@ public class CharacterImageView extends VBox{
 
 	public void setHealth(ProgressBar health) {
 		this.health = health;
+	}
+	
+	public void update() {
+		// getHealth().setProgress(getCharacter().getCurrentHealth() / getCharacter().getMaxHealth());
 	}
 }
