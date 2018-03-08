6a9529c3f304bde918f39287932520f50ff74e61
yart123 <iaroslav.titov@gmail.com>
2018-03-08 16:49:54 -0700

    fixes

--- src/views/controllers/PartyManagementController.java
+++ src/views/controllers/PartyManagementController.java
@@ -667,11 +667,9 @@ public class PartyManagementController implements PlayerController
 		if (ps.getPlayers()[2] != null) player3Image.setImage(ps.getPlayers()[2].getImage());
 		if (ps.getPlayers()[3] != null) player4Image.setImage(ps.getPlayers()[3].getImage());
 		
-//		for (int i = 0; i < 3; i++) {
-//		if(ps.getPlayers()[i] == null) {
-//			hireImage.setVisible(true);
-//		}
-//		}
-		
+			if(ps.getPlayers()[3] == null)
+				hireImage.setVisible(true);
+			else
+				hireImage.setVisible(false);
 	}
 }
